aaa782865429be2132c8f3c18a9f488d7533f528
Patrick <patrickdasilva2509@gmail.com>
2026-06-23 15:41:34 -0300

    refactor: separa funcionalidades em funções

--- controle-estoque.py
+++ controle-estoque.py
@@ -1,70 +1,82 @@
 # Projeto controle de estoque:
-
-def ler_inteiro(msg): # Função para ler um número inteiro do usuário, com tratamento de exceção para garantir que o valor digitado seja válido.
+def ler_inteiro(msg):
     while True:
         try:
             return int(input(msg))
         except ValueError:
             print("Digite um número válido.")
 
+def ler_nome(msg):
+    return input(msg).strip().lower()
 
-estoque = {}    # Um lugar para guardar os produtos.
+def cadastrar_produto():
+    nome = ler_nome("Digite o nome do produto: ")
 
-while True: # Loop infinito para o menu.
-    print ("=== CONTROLE DE ESTOQUE ===\n") # Título do programa
-    print("1 - Cadastrar Produto")     # 1 - Cadastrar produto
-    print("2 - Consultar Estoque")   # 2 - Consultar estoque
-    print("3 - Entrada de Produto")  # 3 - Entrada de produto
-    print("4 - Saída de Produto")  # 4 - Saída de produto
-    print("5 - Sair\n")  # 5 - Sair
-
-    opcao = input("Escolha uma opção: ") # Perguntar a opção para o usuário
-
-
-    if opcao == "1": # Cadastrar produto
-        nome = input("Digite o nome do produto: ").strip().lower() # Perguntar o nome do produto
-
-        if nome in estoque:
+    if nome in estoque:
             print(f"\n Este produto já está cadastrado!\n")
-        else:
-            quantidade = ler_inteiro("Quantidade produto: ") # Perguntar a quantidade do produto
-            estoque[nome] = quantidade # Guardar o produto no estoque, associando o nome à quantidade.
-            print("\nCadastrado com sucesso!\n") # Informar que o produto foi cadastrado com sucesso.
+    else:
+        quantidade = ler_inteiro("Quantidade produto: ")
+        estoque[nome] = quantidade 
+        print("\nCadastrado com sucesso!\n")
+
+def consultar_estoque():
+    if estoque:
+        print("\nEstoque atual:\n")
             
-    elif opcao == "2": # Consultar estoque
-        if estoque:
-            print("\nEstoque atual:\n") # Informar o estoque atual
+        for nome, quantidade in estoque.items():
+            if quantidade >= 0:
+                print(f"Produto: {nome} | Quantidade: {quantidade}\n")
+            else:
+                print(f"Produto: {nome} | PENDENTE: {quantidade} (comprar {abs(quantidade)})")
+    else:
+        print("\nNenhum produto cadastrado no estoque,.\n")
+
+def entrada_de_produto():
+    nome = ler_nome("Digite o nome do produto: ")
+    quantidade = ler_inteiro("Quantidade: ")
+
+    if nome in estoque:
+        estoque[nome] += quantidade
+        print(f"\nEntrou {quantidade} unidades do produto {nome} no estoque.\n")
+    else:
+        print(f"\nProduto {nome} não encontrado no estoque. Cadastre o produto primeiro.\n")
+
+def saida_de_produto():
+    nome = ler_nome("Digite o nome do produto: ")
+    quantidade = ler_inteiro("Quantidade: ")
+
+    if nome in estoque:
+        estoque[nome] -= quantidade
+        print(f"\nSaíram {quantidade} unidades do produto {nome} do estoque.\n")
+    else:
+        print(f"\nProduto {nome} não encontrado no estoque. Cadastre o produto primeiro.\n")
+
+estoque = {}
+
+while True:
+    print ("=== CONTROLE DE ESTOQUE ===\n")
+    print("1 - Cadastrar Produto")
+    print("2 - Consultar Estoque")
+    print("3 - Entrada de Produto")
+    print("4 - Saída de Produto")
+    print("5 - Sair\n")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        cadastrar_produto()
             
-            for nome, quantidade in estoque.items():
-                if quantidade >= 0: # Se a quantidade for maior ou igual a zero, mostrar o nome e a quantidade do produto
-                    print(f"Produto: {nome} | Quantidade: {quantidade}\n")
-                else:
-                    print(f"Produto: {nome} | PENDENTE: {quantidade} (comprar {abs(quantidade)})") # Mostrar o nome e a quantidade do produto
-        else:
-            print("\nNenhum produto cadastrado no estoque,.\n") # Informar que não há produtos cadastrados no estoque.
+    elif opcao == "2":
+        consultar_estoque()
 
-    elif opcao == "3": # Entrada de produto
-        nome = input("Digite Produto: ")
-        quantidade = ler_inteiro("Quantidade: ")
+    elif opcao == "3":
+        entrada_de_produto()   
 
-        if nome in estoque: # Se o nome esta na lista eu devo.
-            estoque[nome] += quantidade # Somo a quantidade do produto digitado se estiver no estoque.
-            print(f"\nEntrou {quantidade} unidades o produto {nome} no estoque.\n") # Informar que o produto entrou no estoque.
-        else: # Se o nome não estiver na lista eu devo.
-            print(f"\nProduto {nome} não encontrado no estoque. Cadastre o produto primeiro.\n")    
-
-    elif opcao == "4": # Saída de produto
-        nome = input("Digite Produto: ") # Perguntar o nome do produto
-        quantidade = ler_inteiro("Quantidade: ") # Perguntar a quantidade do produto
-
-        if nome in estoque:
-            estoque[nome] -= quantidade # Subtrair a quantidade do produto digitado se estiver no estoque.
-            print(f"\nSaíram {quantidade} unidades do produto {nome} do estoque.\n") # Informar que o produto saiu do estoque.
-        else:
-            print(f"\nProduto {nome} não encontrado no estoque. Cadastre o produto primeiro.\n") # Informar que o produto não foi encontrado no estoque.
+    elif opcao == "4":
+        saida_de_produto()
 
     elif opcao == "5": # Sair
         print("Saindo do programa...")
         break
-    else: # Opção inválidade, vi que quando estava escrevendo qualquer coisa que não fosse 1,2,3,4 ou 5 o programa encerrava tudo, então coloquei essa opção para informar o usuário que a opção é inválida, e o programa continua rodando.
-        print("\nOpção inválida. Por favor, escolha uma opção válida.\n")
+    else:
+        print("\nOpção inválida. Por favor, escolha uma opção válida.\n")''
